812d053019ad32b3e682d55e53ed8461edf72ecd
ocean-slang <83712030+ocean-slang@users.noreply.github.com>
2025-09-27 00:34:02 +0000

    Deploying to gh-pages from @ ocean-slang/ocean-slang.github.io@6054970f149f9edfb2d41eb66bc40db85e8a164f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,51 +442,31 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-estimating-eddy-driven-particulate-organic-carbon-flux-from-remote-sensing",
+          title: 'Estimating eddy-driven particulate organic carbon flux from remote sensing',
+          description: "at a dynamic upwelling front",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          description: "Airborne remote sensing of submesoscale dynamics and phytoplankton",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-go-swace",
+          title: 'GO-SWACE',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-modeling-coastal-water-clarity-using-landsat-8-and-sentinel-2",
+          title: 'Modeling Coastal Water Clarity Using Landsat-8 and Sentinel-2',
+          description: "undergrad research work",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
+            },},{id: "projects-nasa-s-mode-bio-optical-calibrations",
+          title: 'NASA S-MODE Bio-optical Calibrations',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
