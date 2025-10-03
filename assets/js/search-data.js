b4906e615b98b9cf59b1f1bbaa5105908887a1fd
ocean-slang <83712030+ocean-slang@users.noreply.github.com>
2025-10-03 22:50:59 +0000

    Deploying to gh-pages from @ ocean-slang/ocean-slang.github.io@a17b97cdf924c16a701faa4e62a65f480c2b3894 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
         },{id: "nav-mentorship",
           title: "mentorship",
           description: "Materials for courses you taught. Replace this text with your description.",
@@ -36,13 +43,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "nav-people",
           title: "people",
