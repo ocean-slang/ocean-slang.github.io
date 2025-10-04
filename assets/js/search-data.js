777dfeb3f3eb0f7f5485b4e18560b11e0c240710
ocean-slang <83712030+ocean-slang@users.noreply.github.com>
2025-10-04 21:04:53 +0000

    Deploying to gh-pages from @ ocean-slang/ocean-slang.github.io@6ef4975a31c7283867f093e59f35fcbfb6c707b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/realcv/";
           },
         },{id: "nav-mentorship",
           title: "mentorship",
