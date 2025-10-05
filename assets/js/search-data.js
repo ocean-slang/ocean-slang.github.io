9d7d9fdd5790392d6597ff515a39b2867c354ae7
ocean-slang <83712030+ocean-slang@users.noreply.github.com>
2025-10-05 20:13:34 +0000

    Deploying to gh-pages from @ ocean-slang/ocean-slang.github.io@dd0b6990e674db237a83288e39500e0d7f6e4306 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,7 +438,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-nasa-s-mode-bio-optical-calibrations",
           title: 'NASA S-MODE Bio-optical Calibrations',
-          description: "a project with a background image",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
